d6e1b1ca2b501f5068574305a6363964de3e1c2d
Taiki ONO <taiks.4559@gmail.com>
2014-06-26 23:09:56 +0900

    Implement ruboty delay

--- lib/ruboty/delay.rb
+++ lib/ruboty/delay.rb
@@ -1,7 +1,36 @@
-require "ruboty/delay/version"
+require 'ruboty'
+require 'ruboty/delay/version'
 
 module Ruboty
-  module Delay
-    # Your code goes here...
+  module Handlers
+    class Delay < Base
+      on(
+        /delay\s+(?<sec>\d+)\s+(?<body>.*)/,
+        name: 'delay',
+        description: 'Execute ruboty command after given seconds',
+      )
+
+      def delay(message)
+        sec, body_ = *message.match_data.captures
+
+        message.instance_eval do
+          define_singleton_method(:body) do
+            body_
+          end
+        end
+
+        Thread.new do
+          sleep sec.to_i
+
+          begin
+            robot.send(:handlers).each do |handler|
+              handler.call(message)
+            end
+          rescue
+            message.reply("after #{sec} seconds, fail with: #{message.body}")
+          end
+        end
+      end
+    end
   end
 end
